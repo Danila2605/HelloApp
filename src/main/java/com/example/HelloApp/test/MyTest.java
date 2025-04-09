016e5016724eb524a026740bd1b4dcbdfe0a5328
Danila <danila.nikulin.05@bk.ru>
2025-04-09 10:17:04 +0400

    start

---
+++ src/main/java/com/example/HelloApp/test/MyTest.java
@@ -0,0 +1,12 @@
+package com.example.HelloApp.test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MyTest {
+    @Test
+    public void testAddition() {
+        assertEquals(2, 1 + 1);
+    }
+}
