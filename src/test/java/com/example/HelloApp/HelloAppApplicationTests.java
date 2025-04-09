90cb6b6ae4e7f6ca1c2cc8a5dfa32a769b0ecd83
Danila <danila.nikulin.05@bk.ru>
2025-04-09 10:49:37 +0400

    add test

--- src/test/java/com/example/HelloApp/HelloAppApplicationTests.java
+++ src/test/java/com/example/HelloApp/HelloAppApplicationTests.java
@@ -3,11 +3,13 @@ package com.example.HelloApp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.Assert.assertEquals;
+
 @SpringBootTest
 class HelloAppApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void testAddition() {
+		assertEquals(2, 1 + 1);
 	}
-
 }
